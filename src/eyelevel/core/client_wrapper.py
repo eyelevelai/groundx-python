dd559eb4a4a3cb89520b684e2f8388c5ed877612
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-06 17:47:14 +0000

    Release 2.0.2

--- src/eyelevel/core/client_wrapper.py
+++ src/eyelevel/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "groundx-python-sdk",
-            "X-Fern-SDK-Version": "2.0.1",
+            "X-Fern-SDK-Version": "2.0.2",
         }
         headers["X-API-Key"] = self.api_key
         return headers
