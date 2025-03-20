06e5d0d6b2d24fdced3b7870c3d863600452d87b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-20 21:28:53 +0000

    Release 2.2.7

--- src/groundx/core/client_wrapper.py
+++ src/groundx/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "groundx",
-            "X-Fern-SDK-Version": "2.2.6",
+            "X-Fern-SDK-Version": "2.2.7",
         }
         headers["X-API-Key"] = self.api_key
         return headers
