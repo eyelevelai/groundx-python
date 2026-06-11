f30fa8658699148b9a0fcbdffe0be7e75cdea2bc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-11 16:29:29 +0000

    Release 3.6.3

--- src/groundx/core/client_wrapper.py
+++ src/groundx/core/client_wrapper.py
@@ -27,12 +27,12 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
-            "User-Agent": "groundx/3.6.2a",
+            "User-Agent": "groundx/3.6.3",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
             "X-Fern-SDK-Name": "groundx",
-            "X-Fern-SDK-Version": "3.6.2a",
+            "X-Fern-SDK-Version": "3.6.3",
             **(self.get_custom_headers() or {}),
         }
         headers["X-API-Key"] = self.api_key
